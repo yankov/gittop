7b66e23ca28c8a67d46da6e0300e4e625b92ab1d
Artem Yankov <artem@badgeville.com>
2013-02-01 21:44:43 -0800

    remove l.js

--- l.js
+++
@@ -1,1 +0,0 @@
- var all_time_leaderboard = [[4, "Artem Yankov", "artem@badgeville.com"], [1, "Artem Yankov", "artem.yankov@gmail.com"]]; var monthly_leaderboard = []; var weekly_leaderboard = [[4, "Artem Yankov", "artem@badgeville.com"], [1, "Artem Yankov", "artem.yankov@gmail.com"]]; var daily_leaderboard = [];
